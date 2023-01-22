79a25e8d3102ce8ce4e72f72cbd1fba8203f3c11
shivapabbu1 <shivaprasad.pabbu@gmail.com>
2023-01-23 01:12:31 +0530

    react-assign two

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+   state={
+    Name:" ",
+    Department:" ",
+    Rating:" ",
+    employe:[]
+  };
+  changeHandler=(s)=>
+  {
+    
+this.setState({[ s.target.name] :s.target.value})
+
+  }
+  submitHandler=(s)=>
+  {
+    s.preventDefault()
+    const empobj={
+      Name :this.state.Name,
+      Department :this.state.Department,
+      Rating :this.state.Rating
+
+    }
+    const arr = this.state.employe;
+    arr.push (empobj)
+    this.setState({employe : arr})
+  }
+
+  render() {
+    return (
+      <div className='maindiv'>
+        <center>
+      <h1> EMPLOYEE FEEDBACK FORM</h1>
+       <form className='myform'>
+       <label>NAME:</label> 
+       <br/>
+        <input type="text"  name="Name" value={this.state.Name} onChange={this.changeHandler}></input> 
+        <br/>
+        <label>DEPARTMENT:</label> 
+        <br/>
+        <input type="text"  name="Department" value={this.state.Department} onChange={this.changeHandler}></input>
+         <br/>
+        <label>RATING:</label>
+         <br/>
+        <input type="number"  name="Rating" value={this.state.Rating} onChange={this.changeHandler}></input> 
+        <br/>
+        <button type='sumbit' onClick={this.submitHandler}> SAVE HERE</button>
+        </form>
+
+        <div className='output'>
+           {this.state.employe.map((value,index)=>
+           {
+            return(
+            <span key={index}>NAME: {value.Name} || Department: {value.Department}  ||  RATING: {value.Rating}</span>
+            )
+           }
+
+           )}
+
+        </div>
+       
+      
+    </center>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
